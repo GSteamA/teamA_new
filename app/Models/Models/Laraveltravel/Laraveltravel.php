05c8f029b083c2daba173e13f64d0ac8b9662249
Zakkey-GH <reedinortoop@gmail.com>
2024-12-22 20:47:24 +0000

    モデルファイルの設定
    docker-compose.ymlを「環境を構築する手段」に統一
    
    コンテナ内でマイグレーションを実行
    
    必要なnpmパッケージをインストール

--- app/Models/Models/Laraveltravel/Laraveltravel.php
+++ app/Models/Models/Laraveltravel/Laraveltravel.php
@@ -9,4 +9,8 @@ class Laraveltravel extends Model
 {
     /** @use HasFactory<\Database\Factories\Models\Laraveltravel\LaraveltravelFactory> */
     use HasFactory;
+    public function laraveltravel()
+    {
+      return $this->hasMany(Laraveltravel::class);
+    }
 }
