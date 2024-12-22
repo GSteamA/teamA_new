56d8da60656340a9efeb9602f5f1b2795fc8975b
Zakkey-GH <reedinortoop@gmail.com>
2024-12-22 19:51:06 +0000

    Laraveltravelのモデルファイルをディレクトリを作成して生成。 また、エラーの対処法としてdocker-compose.ymlで platformにlinux/amd64を加筆。 詳細はイシュー#25を参照。

---
+++ app/Models/Models/Laraveltravel/Laraveltravel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Models\Laraveltravel;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Laraveltravel extends Model
+{
+    /** @use HasFactory<\Database\Factories\Models\Laraveltravel\LaraveltravelFactory> */
+    use HasFactory;
+}
---
+++ database/factories/Models/Laraveltravel/LaraveltravelFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Models\Laraveltravel;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Models\Laraveltravel\Laraveltravel>
+ */
+class LaraveltravelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
