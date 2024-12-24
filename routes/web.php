32866f8c35e39749a981058067dd9f261003cfae
Zakkey-GH <reedinortoop@gmail.com>
2024-12-24 20:55:31 +0000

    アーカイブ（表彰一覧）ページ作成のため、新機能開発ブランチを切った

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+//追加したルート
 Route::get('/laraveltravel', [LaravelTravelController::class, 'index'])->name('laraveltravel.index');
-Route::get('/game-test-harajuku', function () {
-    return view('laraveltravel.Game_test.game_test_harajuku');
-})->name('game_test_harajuku');
+Route::get('/laraveltravel/Game_test/game-test-harajuku', [LaravelTravelController::class, 'show'])->name('game_test_harajuku');
+// Route::get('/laraveltravel/Game_test/game-test-harajuku', function () {
+//     return view('laraveltravel.Game_test.game_test_harajuku');
+// })->name('game_test_harajuku');
 
 //開発中は認証を経由せずにテストするため以下のルートを記載しない
 Route::middleware('auth')->group(function () {
