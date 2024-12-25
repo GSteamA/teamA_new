e09766f85d3a350db5cb16f799ba8fc3df497910
Miria Noda <choko.mn0516@gmail.com>
2024-12-25 11:45:47 +0900

    edit route

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,12 @@ use App\Http\Controllers\Lasvegas\LasvegasController;
 
 
 //ルートではログイン画面を表示（現在は仮でlasvegas/welcome.blade.phpを表示）
+
+//ルートではログイン画面を表示（現在は仮でlasvegas/welcome.blade.phpを表示）
 Route::get('/', function () {
     return view('lasvegas.welcome');
 });
 
-//ログインすると、ララベルトラベルのトップページを表示
-Route::get('/dashboard', [LaravelTravelController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
-//追加したルートRoute::get('/laraveltravel/create', [LaravelTravelController::class, 'create'])->name('laraveltravel.create');
-Route::get('/laraveltravel/Game_test/game-test-harajuku', [LaravelTravelController::class, 'show'])->name('game_test_harajuku');
-
-// Route::get('/laraveltravel/Game_test/game-test-harajuku', function () {
-//     return view('laraveltravel.Game_test.game_test_harajuku');
-// })->name('game_test_harajuku');
 
 // クイズ機能に関するルートをグループ化
 Route::prefix('quiz')->name('Quiz.')->controller(QuizGameController::class)->group(function () {
