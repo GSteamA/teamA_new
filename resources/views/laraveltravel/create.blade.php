0b9de5aa365381bbb17e5db04baddb15da606e6c
Miria Noda <choko.mn0516@gmail.com>
2024-12-25 15:48:34 +0000

    complete display

--- resources/views/laraveltravel/create.blade.php
+++ resources/views/laraveltravel/create.blade.php
@@ -1,25 +1,21 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-      {{ __('アーカイブ') }}
+      {{ __('獲得した記念写真') }}
     </h2>
   </x-slot>
 
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-          @foreach ($laraveltravel as $lrtr)
-          <div class="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
-            <p class="text-gray-800 dark:text-gray-300">{{ $lrtr->lrtr }}</p>
-            <p class="text-gray-600 dark:text-gray-400 text-sm">獲得者: {{ $lrtr->user->name }}</p>
-          </div>
-          @endforeach
-        </div>
-      </div>
-    </div>
-  </div>
+  <div id="shelf"></div>
 
-  <script src="{{asset('js/laraveltravel/create.js')}}"></script>
+  <div id="game-images-container"></div>
+
+  <!-- jQueryをインポート -->
+  <script src="{{asset('js/lasvegas/jquery-2.1.3.min.js')}}"></script>
+  <!-- 画像を取得して表示するjsファイル -->
+  <script src="{{asset('js/showpicture/create.js')}}"></script>
+  <!-- 棚を表示するjsファイル -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+  <script type="module" src="{{asset('js/showpicture/home.shelf.js')}}"></script>
 
 </x-app-layout>
