0a5da162a9543d4a922e7251cf224e6b583fc67d
Miria Noda <choko.mn0516@gmail.com>
2024-12-25 10:27:59 +0000

    delete pass

--- public/js/laraveltravel/config.js
+++ public/js/laraveltravel/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  GOOGLE_MAPS_API_KEY: 'AIzaSyBTULDVdiKocA_Lp5DBD0liJQzsXlXTjug',
+  GOOGLE_MAPS_API_KEY: '',
   GOOGLE_MAP_ID: 'VIDE_API',
   mapCenter: { lat: 36.958962,  lng: -164.216931 }, //マップの初期表示の中心位置
   mapZoom: 3, //マップの初期表示時の拡大率
